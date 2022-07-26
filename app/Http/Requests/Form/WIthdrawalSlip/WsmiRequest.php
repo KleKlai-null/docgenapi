4dae2968b6b735e79a8be53cd5819a783f750ea9
TheCodingCat <mmagallen_180000002192@uic.edu.ph>
2022-07-26 22:02:49 +0800

    namespace fix

--- app/Http/Requests/Form/WIthdrawalSlip/WsmiRequest.php
+++ app/Http/Requests/Form/WIthdrawalSlip/WsmiRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Form\WIthdrawalSlip;
+namespace App\Http\Requests\Form\WithdrawalSlip;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
