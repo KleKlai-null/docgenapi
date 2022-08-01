8b4e7ca8163cf933dfc07baeecb2ed5f3ead060d
TheCodingCat <mmagallen_180000002192@uic.edu.ph>
2022-08-01 12:55:00 +0800

    PDF fix

--- app/Http/Controllers/Api/Form/ReturnSlip/ReturnSlipController.php
+++ app/Http/Controllers/Api/Form/ReturnSlip/ReturnSlipController.php
@@ -132,7 +132,7 @@ class ReturnSlipController extends ApiController
 
             DB::commit();
             
-            DocumentService::generatePDF($data, 'mi', true); //Generate PDF for Backup
+            DocumentService::generatePDF($data, $request->withdrawal_form, true); //Generate PDF for Backup
 
             return $this->sendResponse($data);
 
